98fc5f70c8571cbba7467374745a5fde7ebe245e
Alexey Gladkov <legion@kernel.org>
2025-12-09 14:05:17 +0100

    msgbox: Do not expand container with buttons
    Signed-off-by: Alexey Gladkov <legion@kernel.org>

--- src/plugins/msgbox-main.c
+++ src/plugins/msgbox-main.c
@@ -42,6 +42,8 @@ static struct widget *p_msgbox_create(struct request *req)
 	struct widget *hbox = make_hbox();
 	widget_add(parent, hbox);
 
+	hbox->flex_h = 0;
+
 	int w_id = 1;
 
 	for (size_t i = 0; i < p->num_kv; i++) {
