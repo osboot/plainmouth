aeda18a7ff655a3ca3a645f943a7379239ae29a3
Alexey Gladkov <legion@kernel.org>
2025-12-16 13:32:30 +0100

    Add test case for form plugin
    Signed-off-by: Alexey Gladkov <legion@kernel.org>

--- tests/test-form.sh
+++ tests/test-form.sh
@@ -1,8 +1,14 @@
 #!/bin/sh -efu
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 export PLAINMOUTH_SOCKET="${PLAINMOUTH_SOCKET:-/tmp/plainmouth.sock}"
 
-./plainmouth plugin=form action=create id=w1
+./plainmouth plugin=form action=create id=w1 width=30 height=5 \
+	hbox=start label="Username:" input="legion" hbox=end \
+	hbox=start label="Password:" password="" hbox=end \
+	\
+	button="OK" button="Cancel" \
+	#
 
-#./plainmouth action=wait-result id=w1
-#./plainmouth --quit
+./plainmouth action=wait-result id=w1
+./plainmouth --quit
