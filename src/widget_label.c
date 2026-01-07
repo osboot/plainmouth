0662c20193dd137cb4389753b0ad84a791ba2a52
Alexey Gladkov <legion@kernel.org>
2026-01-07 17:46:03 +0100

    widget_label: Allow to use multiline values
    The current implementation is too simple and does not allow it to be
    used to output text with more than one line. If there are line breaks in
    the string, it is not possible to correctly calculate the size of the
    window to fit the text.
    
    The new implementation correctly calculates nlines and ncols with the
    same interface. The advantage of the new implementation is that it can
    be used to implement other widgets.
    
    Signed-off-by: Alexey Gladkov <legion@kernel.org>

--- src/widget_label.c
+++ src/widget_label.c
@@ -10,28 +10,73 @@
 #include <curses.h>
 
 #include "macros.h"
+#include "warray.h"
 #include "widget.h"
 
 struct widget_label {
-	wchar_t *text;
+	struct warray lines;
+	int ncols;
 };
+
+static void label_init_lines(struct widget_label *st, const wchar_t *text)
+{
+	warray_init(&st->lines);
+	st->ncols = 0;
+
+	const wchar_t *s = text;
+	while (*s) {
+		const wchar_t *e = wcschr(s, L'\n');
+		if (!e)
+			e = s + wcslen(s);
+
+		size_t len = (size_t) (e - s);
+		warray_push(&st->lines, s, len);
+
+		int line_width = 0;
+		for (size_t i = 0; i < len; i++) {
+			int w = wcwidth(s[i]);
+			if (w > 0)
+				line_width += w;
+		}
+		st->ncols = MAX(st->ncols, line_width);
+
+		s = (*e == L'\n') ? e + 1 : e;
+	}
+
+	if (*text && text[wcslen(text) - 1] == L'\n')
+		warray_push(&st->lines, L"", 0);
+}
 
 static void label_measure(struct widget *w)
 {
-	w->min_h = 1;
-	w->min_w = (int) wcslen(w->state.label->text);
+	struct widget_label *state = w->state.label;
+
+	w->min_h = (int) state->lines.size;
+	w->min_w = state->ncols;
 }
 
 static void label_render(struct widget *w)
 {
-	mvwaddwstr(w->win, 0, 0, w->state.label->text);
+	struct widget_label *state = w->state.label;
+
+	int maxy = getmaxy(w->win);
+	int maxx = getmaxx(w->win);
+
+	werase(w->win);
+
+	for (int y = 0; y < maxy && (size_t) y < state->lines.size; y++) {
+		const wchar_t *line = warray_get(&state->lines, (size_t) y);
+		if (line)
+			mvwaddnwstr(w->win, y, 0, line, maxx);
+	}
 }
 
 static void label_free(struct widget *w)
 {
-	if (w->state.label) {
-		free(w->state.label->text);
-		free(w->state.label);
+	struct widget_label *state = w->state.label;
+	if (state) {
+		warray_free(&state->lines);
+		free(state);
 	}
 }
 
@@ -50,7 +95,7 @@ struct widget *make_label(const wchar_t *line)
 		return NULL;
 	}
 
-	state->text = wcsdup(line ?: L"");
+	label_init_lines(state, line);
 
 	w->state.label = state;
 	w->color_pair  = COLOR_PAIR_WINDOW;
